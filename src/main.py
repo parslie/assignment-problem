5ef2fbf4a844fbbd50745cfd4caa1b3841d7d195
Viktor Holta <viktor.holta@outlook.com>
2022-08-03 13:47:39 +0200

    Create function for generating random cost matrix

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
+import numpy as np
+import random
+
+
+def generate_cost_matrix(row_count: int, col_count: int, max_cost: float = 10) -> np.ndarray:
+    matrix = np.ndarray((row_count, col_count))
+    for row in range(0, row_count):
+        for col in range(0, col_count):
+            matrix[row, col] = random.random() * max_cost
+    return matrix
+
+
 if __name__ == "__main__":
-    pass
+    cost_matrix = generate_cost_matrix(10, 10)
+    print(cost_matrix)
