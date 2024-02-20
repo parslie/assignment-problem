aeaffaa5e87e512214ca9b60ff1901786e94c461
Viktor Holta <viktor.holta@outlook.com>
2024-02-20 13:28:37 +0100

    Add test for whole hungarian method

--- src/test/test_hungarian.py
+++ src/test/test_hungarian.py
@@ -15,3 +15,44 @@ def test_reduce():
         [1, 0, 6],
         [0, 3, 2],
     ], dtype=float).tolist()
+
+    costs = numpy.array([
+        [3, 5, 0],
+        [2, 0, 7],
+        [5, 4, 0],
+    ], dtype=float)
+
+    reduce_cost_matrix(costs)
+    assert costs.tolist() == numpy.array([
+        [1, 5, 0],
+        [0, 0, 7],
+        [3, 4, 0],
+    ], dtype=float).tolist()
+
+    costs = numpy.array([
+        [3, 5, 2],
+        [2, 1, 7],
+        [5, 4, 1],
+    ], dtype=float)
+
+    reduce_cost_matrix(costs)
+    assert costs.tolist() == numpy.array([
+        [0, 3, 0],
+        [0, 0, 6],
+        [3, 3, 0],
+    ], dtype=float).tolist()
+
+
+def test_algorithm():
+    costs = numpy.array([
+        [3, 5, 1],
+        [2, 1, 7],
+        [1, 4, 3],
+    ], dtype=float)
+
+    assignments = hungarian_algorithm(costs)
+    assert assignments == [
+        (0, 2),
+        (1, 1),
+        (2, 0),
+    ]
